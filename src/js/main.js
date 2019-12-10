ee0ffef24790b1c0b44eb3fcd38d70082c5f1979
Tigran AL <tigran@appliedlabs.io>
2019-12-10 16:05:54 +0500

    icon-headline-description, icon-headline-description-cta

--- src/js/main.js
+++ src/js/main.js
@@ -45,3 +45,41 @@ $('.article-list__tabs-buttons button').on('click', (e) => {
 
   $activeButton.removeClass('is-active');
 });
+
+$('.js-slider').each((index, element) => {
+  let $this = $(element);
+  let sliderInfinite = $this.data('infinite') !== undefined ? $this.data('infinite') : false;
+  let sliderArrows = $this.data('arrows') !== undefined ? $this.data('arrows') : true;
+  let sliderDots = $this.data('dots') !== undefined ? $this.data('dots') : false;
+  let slidesToShowDesktop = $this.data('slides-desktop') !== undefined ? $this.data('slides-desktop') : 4;
+  let slidesToShowTablet = $this.data('slides-tablet') !== undefined ? $this.data('slides-tablet') : 2;
+  let slidesToShowMobile = $this.data('slides-mobile') !== undefined ? $this.data('slides-mobile') : 1;
+  let sliderSpeed = $this.data('speed') !== undefined ? $this.data('speed') : 500;
+  let sliderAutoPlay = $this.data('autoplay') !== undefined ? $this.data('autoplay') : false;
+  let sliderAutoPlaySpeed = $this.data('autoplay-speed') !== undefined ? $this.data('autoplay-speed') : 2000;
+
+  $this.slick({
+    infinite: sliderInfinite,
+    slidesToShow: slidesToShowDesktop,
+    slidesToScroll: 1,
+    arrows: sliderArrows,
+    dots: sliderDots,
+    autoplay: sliderAutoPlay,
+    speed: sliderSpeed,
+    autoplaySpeed: sliderAutoPlaySpeed,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: slidesToShowTablet,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: slidesToShowMobile,
+        },
+      },
+    ],
+  });
+});
